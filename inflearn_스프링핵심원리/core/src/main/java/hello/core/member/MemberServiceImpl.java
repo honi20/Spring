e4d3414b06db1b38e91a0641adbacdd017c47e31
honi20 <1594cyh@gmail.com>
2023-05-02 04:47:24 +0900

    infl

--- inflearn_스프링핵심원리/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ inflearn_스프링핵심원리/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -23,3 +28,4 @@ public class MemberServiceImpl implements MemberService{
         return memberRepository;
     }
 }
+
